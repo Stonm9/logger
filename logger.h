3e58a98c5efb1766ba2db9f3a54c79ff8a328225
Denis Petrov <mebelholm@yandex.ru>
2024-12-01 08:33:53 -0300

    edit logger

--- logger.h
+++ logger.h
@@ -7,16 +7,21 @@ class Logger {
   std::ofstream outFile;
   bool lan = false;  // Если True значит будем работать по сети
  public:
-  Logger(bool value) : consolOrFile(value), outFile("log.txt", std::ios::app) {}  // Конструктор по умолчанию
-  Logger(const std::string& str, const bool value) : consolOrFile(value), outFile(str, std::ios::app) {}  // Конструктор где можно задать имя файла
-  Logger(const std::string& str, const bool value, const bool valueLan): consolOrFile(value), outFile(str, std::ios::app), lan(valueLan) {}
+  // Конструктор по умолчанию
+  Logger(bool value) : consolOrFile(value), outFile("log.txt", std::ios::app) {}  
+  // Конструктор где можно задать имя файла
+  Logger(const std::string& str, const bool value) : consolOrFile(value), outFile(str, std::ios::app) {}  
+  // Конструктор где можно задать имя файла и что запись будет производится по сети
+  Logger(const std::string& str, const bool value, const bool valueLan): consolOrFile(value), outFile(str, std::ios::app), lan(valueLan) {} 
   
   void Write(const std::string& str) {
     if (consolOrFile == true) {  // Показать в консоль
       std::cout << str << '\n';
     } else {  // Запись в файл
       if (lan == true) {
+        // -----------------------------------------
         // Тут содержиться код записи в файл по сети
+        // -----------------------------------------
       } else {
         if (!outFile) {
           std::cerr << "Не удалось открыть файл для записи";
